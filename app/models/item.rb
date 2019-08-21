28de87a1e062b83c003cada807d13336d7f5432b
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-08-21 09:03:44 -0300

    Install Annotate gem

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  price_new   :decimal(, )
+#  price_sale  :decimal(, )
+#  status      :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint
+#
+# Indexes
+#
+#  index_items_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
+
 class Item < ApplicationRecord
   belongs_to :category
 end
